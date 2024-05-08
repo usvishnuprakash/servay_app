212947741b0630565ed50b722dc466981be8808b
vishnuprakash <usvishnuprakash>
2024-05-08 14:47:59 +0530

    initial commit has done

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        borderColor: "rgb(2, 173, 187)",
+      },
+    },
+  },
+  plugins: [],
+};
